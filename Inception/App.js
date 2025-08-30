ee5adcb31f2909be3e05dc11a3cdafbf9c9fb7c6
Pratika Gupta <pratika.gupta008@gmail.com>
2025-08-30 12:32:38 +0530

    episode-01

---
+++ Inception/App.js
@@ -0,0 +1,44 @@
+/**
+ * <div id = "parent"
+ *     <div id = "child"
+ *          <h1>I'm h1 tag</div>
+ *          <h2>I'm h1 tag</div>
+ *     </div>
+ *     <div id = "child2"
+ *          <h1>I'm h1 tag</div>
+ *          <h2>I'm h1 tag</div>
+ *     </div>
+ * </div>
+ * 
+ * ReactElement(Object) => HTML(Browser Understands) 
+ *
+ */
+
+
+const parent = React.createElement("div", {id: "parent"}, [
+    React.createElement("div", {id: "child"}, [
+        React.createElement("h1", {}, "I'm an h1 tag"),
+        React.createElement("h2", {}, "I'm an h2 tag"),
+
+    ]),
+        React.createElement("div", {id: "child2"}, [
+        React.createElement("h1", {}, "I'm an h1 tag"),
+        React.createElement("h2", {}, "I'm an h2 tag"),
+
+    ])
+]);
+
+//JSX
+
+
+// const heading = React.createElement(
+//     "h1", 
+//     {id: "heading", xyz: "abc"}, 
+//     "Hello World from React!"
+// );
+
+console.log(parent); // object
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
